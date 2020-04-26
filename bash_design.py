3ec9c5ca61dfc598735fc1db863121a4af6e8920
Anastasia Razdaibiedina <37491405+arazd@users.noreply.github.com>
2020-04-25 22:14:28 -0400

    Update bash_design.py

--- bash_design.py
+++ bash_design.py
@@ -8,7 +8,7 @@ dir=os.getcwd()
 fname1=str(sys.argv[1])      # .fsa file in the same folder with this script
 PAM1=str(sys.argv[2])        # usually NGG or NG; should be sequence of nucleotides (AGG, CAT etc.). Put N for any nucleotide
 region1=int(sys.argv[3])     # usually 20
-mut_window1=int(sys.argv[4]) # usually 12
+mut_window1=int(sys.argv[4]) # mutation window (in base-pairs), usually 12
 
 run_design(dir, fname1, PAM1, region1, mut_window1)
 print('Done!')
